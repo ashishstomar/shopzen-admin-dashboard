d7cdeb5e29d035036cff67796769509eabf86d71
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2025-03-17 19:02:05 +0530

    feat: add lazy loading and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {Suspense, lazy} from 'react'
-import Loading from "./components/loading"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loading from "./components/Loading";
 
-const Dashboard = lazy(()=> import ("./pages/dashboard"));
-const Products = lazy(()=> import ("./pages/products"));
-const Transactions = lazy(()=> import ("./pages/transaction"));
-const Customers = lazy(()=> import ("./pages/customers"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Transactions = lazy(() => import("./pages/Transaction"));
+const Customers = lazy(() => import("./pages/Customers"));
+
+const AddProduct = lazy(() => import("./pages/manage/AddProduct"));
+const ManageProduct = lazy(() => import("./pages/manage/ManageProduct"));
+const ManageTransaction = lazy(
+  () => import("./pages/manage/ManageTransaction")
+);
+const BarCharts = lazy(() => import("./pages/charts/BarCharts"));
+const PieCharts = lazy(() => import("./pages/charts/PieCharts"));
+const LineCharts = lazy(() => import("./pages/charts/LineCharts"));
+const Coupon = lazy(() => import("./pages/apps/Coupon"));
+const Stopwatch = lazy(() => import("./pages/apps/Stopwatch"));
+const Toss = lazy(() => import("./pages/apps/Toss"));
 
 const App = () => {
   return (
     <Router>
-      <Suspense fallback={<Loading/>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
-          <Route  path="/admin/dashboard" element={<Dashboard/>} />
-          <Route  path="/admin/products" element={<Products/>} />
-          <Route  path="/admin/tansaction" element={<Transactions/>} />
-          <Route  path="/admin/customers" element={<Customers/>} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/transaction" element={<Transactions />} />
+          <Route path="/admin/customer" element={<Customers />} />
+
+          {/*Charts goes here */}
+          <Route path="/admin/chart/bar" element={<BarCharts />} />
+          <Route path="/admin/chart/pie" element={<PieCharts />} />
+          <Route path="/admin/chart/line" element={<LineCharts />} />
+
+          {/*Apps goes here */}
+          <Route path="/admin/app/coupon" element={<Coupon />} />
+          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          <Route path="/admin/app/toss" element={<Toss />} />
+          {/* Manage */}
+          <Route path="/admin/product/add" element={<AddProduct />} />
+          <Route path="/admin/product/:id" element={<ManageProduct />} />
+          <Route
+            path="/admin/transaction/:id"
+            element={<ManageTransaction />}
+          />
         </Routes>
       </Suspense>
     </Router>
